4d8a4018518f0872e46827d68d43980f6f4f3b5f
Wagner Lobo <wagner.lobobr@gmail.com>
2024-01-09 22:46:21 -0300

    Leeting Code

--- remove_duplicates_from_sorted_arrays.py
+++ remove_duplicates_from_sorted_arrays.py
@@ -1,18 +1,39 @@
 '''
 '''
 def removeDuplicates(nums):
-    return print(len(list(set(nums))))
+    i = 0
+    j = 1
+    if len(nums) == 1:
+        return len(nums)
+    elif len(nums) == 2:
+        if nums[0] == nums[1]:
+            nums.remove(nums[1])
+            return 1
+        else:
+            return len(nums)
+    else:
+        while i < len(nums) and j < (len(nums) - 1):
+            if nums[i] == nums[j]:
+                nums.remove(nums[j])
+                j = i+1
+                i += 1
+            else:
+                i += 1
+                j = i+1
+        return (len(nums))
 
 
-nums = [1,1,2]
+#nums = [0,0,1,1,1,2,2,3,3,4] 
+nums = [1,2,2]
+#nums = [1,1,1]       
 removeDuplicates(nums)
 
 
 '''
 Example 1:
 
-Input: nums = [1,1,2]
-Output: 2, nums = [1,2,_]
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 2, nums = [0,1,2,3,4]
 Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
 It does not matter what you leave beyond the returned k (hence they are underscores).
 Example 2:
